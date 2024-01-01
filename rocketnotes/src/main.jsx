91818f0484533cda0b1bce77d07a3a2c4fe10dc6
iCaioReis <caioreis3186@gmail.com>
2024-01-01 19:54:20 -0300

    Testing CSS on react

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Datails } from './pages/Datails.jsx'
+import { Datails } from './pages/Datails'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- rocketnotes/src/pages/Datails/index.jsx
+++ rocketnotes/src/pages/Datails/index.jsx
@@ -1,3 +1,5 @@
+import './styles.css'
+
 export function Datails(){
   
   return(
