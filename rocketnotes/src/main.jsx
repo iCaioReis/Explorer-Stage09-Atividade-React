f57777a440e3231796618867f528cba075550ee6
iCaioReis <caioreis3186@gmail.com>
2024-01-03 11:42:56 -0300

    Global styles

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
 
 import theme from './styles/theme';
 
@@ -9,6 +10,7 @@ import { Datails } from './pages/Datails'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme= { theme } >
+      <GlobalStyles/>
       <Datails />
     </ThemeProvider>
   </React.StrictMode>,
