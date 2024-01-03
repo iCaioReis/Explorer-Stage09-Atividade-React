3e07a86c0220439a055fa34e89f8e4361ad0af7a
iCaioReis <caioreis3186@gmail.com>
2024-01-03 20:39:32 -0300

    Add fonts

--- rocketnotes/src/styles/global.js
+++ rocketnotes/src/styles/global.js
@@ -10,6 +10,14 @@ export default createGlobalStyle`
     body{
         background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800 };
         color: ${({ theme }) => theme.COLORS.WHITE };
+
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button, textarea {
+        font-family: 'Roboto Slab', serif;
+        font-size: 16px;
+        outline: none;
     }
     
     a {
@@ -23,4 +31,4 @@ export default createGlobalStyle`
     button:hover, a:hover{
         filter: brightness(0.9);
     }
-`
+`;
