6e156f6118d24a46e7c5e79f0c20385dbc41e9da
iCaioReis <caioreis3186@gmail.com>
2024-01-03 21:48:56 -0300

    Add Button Component

--- rocketnotes/src/pages/Datails/index.jsx
+++ rocketnotes/src/pages/Datails/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from './styles.js';
+import { Button } from '../../components/Button';
 
 export function Datails(){
   
   return(
     <Container>
       <h1>Hello Word</h1>
+      <Button/>
     </Container>
   )
 }
