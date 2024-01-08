704ce34428cfce6999c3bcf093e0489d9c121b8b
iCaioReis <caioreis3186@gmail.com>
2024-01-07 22:35:40 -0300

    Styling Profile Header

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,8 +1,16 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header (){
     return (
         <Container>
+            <Profile>
+                <img src="https://github.com/iCaioReis.png" alt="Imagem do usuário" />
+
+                <div>
+                    <span>Bem-vindo,</span>
+                    <strong>Caio Reis</strong>
+                </div>
+            </Profile>
 
         </Container>
     );
