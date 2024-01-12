053602c90ce54d9c1205ea700ca8dafb1106931d
iCaioReis <caioreis3186@gmail.com>
2024-01-11 22:15:45 -0300

    Created Component ButtonText

--- rocketnotes/src/pages/Datails/index.jsx
+++ rocketnotes/src/pages/Datails/index.jsx
@@ -3,12 +3,16 @@ import { Container, Links } from './styles.js';
 import { Header } from '../../components/Header/index.jsx';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section/index.jsx';
+import { ButtonText } from '../../components/ButtonText/index.jsx';
 
 export function Datails(){
   
   return(
     <Container>
       <Header></Header>
+
+    <ButtonText title="Excluir Nota"></ButtonText>
+
       <Section title={"Links úteis"}>
         <Links>
           <li><a href="#">Item 1</a></li>
