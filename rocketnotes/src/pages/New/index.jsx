97c4cd217e1e3fa12071f7cefc86e68a1f770e47
iCaioReis <caioreis3186@gmail.com>
2024-03-25 16:33:58 -0300

    Consuming API to create new note

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useNavigate } from 'react-router-dom';
+
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Section } from '../../components/Section';
@@ -8,14 +10,22 @@ import { Button } from '../../components/Button';
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 
+import { api } from '../../services/api';
+
 import { Container, Form } from "./styles";
 
 export function New() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+
     const [links, setLinks] = useState([]);
     const [newLink, setNewLink] = useState("");
 
     const [tags, setTags] = useState([]);
     const [newTag, setNewTag] = useState("");
+
+    const navigate = useNavigate();
 
     function handleAddLink(){
         setLinks(prevState => [...prevState, newLink]);
@@ -29,10 +39,28 @@ export function New() {
         setTags(prevState => [...prevState, newTag]);
         setNewTag("");
     }
+
     function handleRemoveTag(deleted){
         setTags(prevState => prevState.filter(link => link !== deleted));
     }
 
+    async function handleNewNote(){
+        if (newTag){
+            return alert("Existe uma Tag não salva. Clique para salvar ou deixe o campo vazio.");
+        };
+        if (newLink){
+            return alert("Existe um link não salvo. Clique para salvar ou deixe o campo vazio.");
+        };
+        await api.post("/notes", {
+            title,
+            description,
+            links,
+            tags
+        });
+
+        alert("Nota criada com sucesso!")
+        navigate("/");
+    }
 
     return (
         <Container>
@@ -45,8 +73,14 @@ export function New() {
                         <Link to={"/"}>voltar</Link>
                     </header>
 
-                    <Input placeholder="Título" />
-                    <Textarea placeholder="Observações" />
+                    <Input 
+                        placeholder="Título"
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <Textarea 
+                        placeholder="Observações"
+                        onChange={e => setDescription(e.target.value)} 
+                    />
 
                     <Section title={"Links úteis"}>
                         {
@@ -88,7 +122,10 @@ export function New() {
                         </div>
                     </Section>
 
-                    <Button title={"Salvar"} />
+                    <Button 
+                        title={"Salvar"}
+                        onClick={handleNewNote}
+                    />
                 </Form>
             </main>
 
