49e1aa5e672591bce5f8c4fb43f93d9ba6c96efd
iCaioReis <caioreis3186@gmail.com>
2024-03-31 19:54:37 -0300

    Handling unavailable routes

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -6,9 +6,16 @@ import { api } from "../../services/api";
 import { Container, Profile, Logout } from "./styles";
 
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+import { useNavigate } from "react-router-dom";
 
 export function Header (){
     const { signOut, user } = useAuth();
+    const navigation = useNavigate();
+
+    function handleSignOut(){
+        navigation("/")
+        signOut();
+    }
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
     return (
@@ -22,7 +29,7 @@ export function Header (){
                 </div>
             </Profile>
 
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine/>
             </Logout>
 
